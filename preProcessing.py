4530445d5777c840bd1e2d00afa7171162517880
MaxLSB <m.lasbordes@gmail.com>
2024-07-18 16:23:25 +0200

    PreProcessing is done

--- preProcessing.py
+++ preProcessing.py
@@ -5,31 +5,33 @@ import random
 from torch.utils.data import Dataset
 
 class BERTPreProcessing(Dataset):
-    def __init__(self, dataset, tokenizer, max_length):
+    def __init__(self, dataset, tokenizer, seq_length=64):
         self.dataset = dataset
         self.tokenizer = tokenizer
-        self.max_length = max_length
+        self.seq_length = seq_length
         self.vocab_size = len(self.tokenizer.vocab)
        
     def __len__(self):
         return len(self.dataset)
     
     def __getitem__(self, item):
-        token_embeddings = []
-        segment_embeddings = []
-        
+       
         sentenceA, sentenceB, is_next = self.get_label_element(item)
-        # a faire : Liste avec 0 pour les éléments inchangés et l'id du token masqué pour le training
+        
+        # We mask the tokens in the sentence and get the corresponding mask_ids
         masked_sentenceA, mask_idsA = self.get_masked(sentenceA)
         masked_sentenceB, mask_idsB = self.get_masked(sentenceB)
         
-        token_embeddings.extend(masked_sentenceA)
-        token_embeddings.extend(masked_sentenceB[1:])
+        # We concatenata the two sentences for each embedding (we don't take into account the [CLS] )
+        token_embeddings = (masked_sentenceA + masked_sentenceB[1:])[:self.seq_length]
+        mask_ids = (mask_idsA + mask_idsB[1:])[:self.seq_length]
+        segment_embeddings = ([1]*len(masked_sentenceA) + [2]*len(masked_sentenceB[1:]))[:self.seq_length]
         
-        segment_embeddings.extend([0]*len(masked_sentenceA))
-        segment_embeddings.extend([1]*len(masked_sentenceB[1:]))
-        
-        mask_ids = mask_idsA + mask_idsB[1:]
+        # We pad the embeddings to the max length if needed otherwise padding = [] and nothing changes
+        padding = [self.tokenizer.vocab['[PAD]'] for _ in range(self.seq_length - len(token_embeddings))]
+        token_embeddings.extend(padding)
+        mask_ids.extend(padding)
+        segment_embeddings.extend([0]*len(padding))
         
         output = {"token_embeddings" : token_embeddings,
                   "segment_embeddings" : segment_embeddings,
@@ -38,10 +40,9 @@ class BERTPreProcessing(Dataset):
         
         return output
         
-        
     def get_masked(self, sentence):
         # tokens is the corresponding tokenized sentence
-        tokens = self.tokenizer(sentence, max_length=self.max_length, truncation=True)['input_ids']
+        tokens = self.tokenizer(sentence, max_length=self.seq_length, truncation=True)['input_ids']
         mask_ids = [0]*len(tokens)
         # We don't take into account the [CLS] and [SEP] tokens
         for i in range(1,len(tokens)-1):
@@ -71,14 +72,10 @@ class BERTPreProcessing(Dataset):
             
     def get_random_element(self):
         index = random.randint(0, len(self.dataset)-1)
-        return self.get_index_element(index)[1]
+        return self.dataset[index][1]
 
 
-
-
-
-dataset = load_data('data/processed_data.json')
-tokenizer = load_tokenizer('tokenizers/vocab.txt')
-a = random.choice(list(tokenizer.vocab.keys()))
-
-#print(tokenizer(dataset[0], max_length=10, truncation=True, padding='max_length'))
+# dataset = load_data('data/processed_data.json')
+# tokenizer = load_tokenizer('tokenizers/')
+# Dataset = BERTPreProcessing(dataset, tokenizer)
+# print(Dataset.__getitem__(0))
